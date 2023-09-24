4ea358d806657c6099b6eb537cd50971bf22e9a0
uyelnico <145890121+uyelnico@users.noreply.github.com>
2023-09-24 03:47:15 -0300

    Create inicio.java

---
+++ inicio.java
@@ -0,0 +1,17 @@
+package proceso;
+
+import java.awt.EventQueue;
+
+public class inicio {
+
+    public static void main(String[] args) {
+        EventQueue.invokeLater(() -> {
+            try {
+                logeo frame = new logeo();
+                frame.setVisible(true);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        });
+    }
+}
