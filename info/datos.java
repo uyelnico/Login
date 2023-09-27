744288ca753690b22304a6511185e14d2ea02ef7
Nicolay Chiazzaro Tosquellas <145890121+uyelnico@users.noreply.github.com>
2023-09-27 14:45:35 -0300

    Create datos.java

---
+++ info/datos.java
@@ -0,0 +1,17 @@
+package info;
+
+public class datos {
+
+	public static final String USUARIO_ESTABLECIDO = "Nicolay";
+	public static final String CONTRASEÑA_ESTABLECIDA = "123";
+	
+	public static final String CEDULA_ESTABLECIDA = "XXXXXXXX";
+	public static final String NACIONALIDAD_ESTABLECIDA = "Uruguaya"; 
+	
+	public static final String FOTO_PERFIL = "";
+}
+
+/**
+ * @author Nicolay Chiazzaro
+ *
+ */
